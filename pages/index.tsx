64b49c0da439707689db850e837437170a683b8d
patricklouback <patrickmlouback@gmail.com>
2022-09-12 13:51:44 -0300

    evoluindo o projeto quiz do curso de nextJS

---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import Questao from '../components/Questao'
+import QuestaoModel from '../model/questao'
+import RespostaModel from '../model/resposta'
+
+const questaoMock = new QuestaoModel(1, "Melhor cor?", [
+  RespostaModel.errada('Verde'),
+  RespostaModel.errada('Vermelha'),
+  RespostaModel.errada('Azul'),
+  RespostaModel.certa('Preta')
+])
+
+export default function Home() {
+
+  const [questao, setQuestao] = useState(questaoMock)
+
+  function respostaFornecida(indice:number) {
+    console.log(indice)
+  }
+  return (
+    <div style={
+      {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh'
+      }
+    }>
+
+    <Questao valor={questao}
+    respostaFornecida={respostaFornecida}/>
+    </div>
+  )
+}
