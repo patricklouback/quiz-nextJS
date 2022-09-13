f2ef302e40a32667b52dc82f0f77bf9f1bc901de
patricklouback <patrickmlouback@gmail.com>
2022-09-13 13:03:26 -0300

    finalizando projeto quiz do curso de reactJS

--- components/Resposta.tsx
+++ components/Resposta.tsx
@@ -10,25 +10,21 @@ interface RespostaProps {
 }
 
 export default function Resposta(props: RespostaProps) {
-
     const resposta = props.valor
-
+    const respostaRevelada = resposta.revelada ? styles.respostaRevelada : ''
     return (
         <div className={styles.resposta}
-            onClick={() => { props.respostaFornecida(props.indice) }}>
-            <div className={styles.conteudoResposta}>
-                {!resposta.revelada ? (
+            onClick={() => props.respostaFornecida(props.indice)}>
+            <div className={`${respostaRevelada} ${styles.conteudoResposta}`}>
                 <div className={styles.frente}>
                     <div className={styles.letra}
-                        style={{backgroundColor: props.corFundoLetra}}
-                    >
+                        style={{ backgroundColor: props.corFundoLetra }}>
                         {props.letra}
                     </div>
                     <div className={styles.valor}>
                         {resposta.valor}
                     </div>
-                </div> 
-                ) : (
+                </div>
                 <div className={styles.verso}>
                     {resposta.certa ? (
                         <div className={styles.certa}>
@@ -37,13 +33,11 @@ export default function Resposta(props: RespostaProps) {
                         </div>
                     ) : (
                         <div className={styles.errada}>
-                            <div>A resposta está errada...</div>
+                            <div>A resposta informada está errada...</div>
                             <div className={styles.valor}>{resposta.valor}</div>
                         </div>
                     )}
                 </div>
-                )}
-
             </div>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,73 @@
-import { useState } from 'react'
-import Questao from '../components/Questao'
+import { useEffect, useState } from 'react'
+import Questionario from '../components/Questionario'
 import QuestaoModel from '../model/questao'
-import RespostaModel from '../model/resposta'
+import { useRouter } from 'next/router'
 
-const questaoMock = new QuestaoModel(1, "Melhor cor?", [
-  RespostaModel.errada('Verde'),
-  RespostaModel.errada('Vermelha'),
-  RespostaModel.errada('Azul'),
-  RespostaModel.certa('Preta')
-])
+const BASE_URL = 'http://localhost:3000/api'
 
 export default function Home() {
+  const router = useRouter()
 
-  const [questao, setQuestao] = useState(questaoMock)
+  const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([])
+  const [questao, setQuestao] = useState<QuestaoModel>()
+  const [respostasCertas, setRespostasCertas] = useState<number>(0)
 
-  function respostaFornecida(indice:number) {
-    setQuestao(questao.responderCom(indice))
+  async function carregarIdsDasQuestoes() {
+    const resp = await fetch(`${BASE_URL}/questionario`)
+    const idsDasQuestoes = await resp.json()
+    setIdsDasQuestoes(idsDasQuestoes)
   }
 
-  function tempoEsgotado() {
-    if (questao.naoRespondida){
-      setQuestao(questao.responderCom(-1))
-    }
+  async function carregarQuestao(idQuestao: number) {
+    const resp = await fetch(`${BASE_URL}/questoes/${idQuestao}`)
+    const json = await resp.json()
+    const novaQuestao = QuestaoModel.criarUsandoObjeto(json)
+    setQuestao(novaQuestao)
   }
 
-  return (
-    <div style={
-      {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh'
+  useEffect(() => {
+    carregarIdsDasQuestoes()
+  }, [])
+
+  useEffect(() => {
+    idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0])
+  }, [idsDasQuestoes])
+
+  function questaoRespondida(questaoRespondida: QuestaoModel) {
+    setQuestao(questaoRespondida)
+    const acertou = questaoRespondida.acertou
+    setRespostasCertas(respostasCertas + (acertou ? 1 : 0))
+  }
+
+  function idProximaPergunta() {
+    const proximoIndice = idsDasQuestoes.indexOf(questao.id) + 1
+    return idsDasQuestoes[proximoIndice]
+  }
+
+  function irPraProximoPasso() {
+    const proximoId = idProximaPergunta()
+    proximoId ? irPraProximaQuestao(proximoId) : finalizar()
+  }
+
+  function irPraProximaQuestao(proximoId: number) {
+    carregarQuestao(proximoId)
+  }
+
+  function finalizar() {
+    router.push({
+      pathname: "/resultado",
+      query: {
+        total: idsDasQuestoes.length,
+        certas: respostasCertas
       }
-    }>
+    })
+  }
 
-    <Questao valor={questao}
-    tempoPraResposta={5}
-    respostaFornecida={respostaFornecida}
-    tempoEsgotado={tempoEsgotado}/>
-    </div>
-  )
+  return questao ? (
+    <Questionario
+      questao={questao}
+      ultima={idProximaPergunta() === undefined}
+      questaoRespondida={questaoRespondida}
+      irPraProximoPasso={irPraProximoPasso} />
+  ) : false
 }
